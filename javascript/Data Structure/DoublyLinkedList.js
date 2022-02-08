9622990e49b02d658d33bc1cbff97990c285235d
wish1030 <wish@khu.ac.kr>
2022-02-08 13:48:21 +0900

    [Data Structure] Doubly Linked List

---
+++ javascript/Data Structure/DoublyLinkedList.js
@@ -0,0 +1,68 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  //연결리스트의 맨 뒤에 추가
+  append(value) {
+    const node = new Node(value);
+    //list가 비었을때
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      //list에 node가 존재할때
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+    this.size += 1;
+    return this;
+  }
+
+  //연결리스트의 맨 앞을 삭제
+  deleteHead() {
+    //연결리스트가 비었을때
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    const deletedHead = this.head;
+    //리스트에 노드가 하나밖에 없을때
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+      this.head.prev = null;
+    }
+    this.size -= 1;
+    return deletedHead.value;
+  }
+
+  print() {
+    let iter = this.head;
+    let print = "";
+    while (iter !== null) {
+      print += `${iter.value} `;
+      iter = iter.next;
+    }
+    console.log(print);
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+}
+
+export default DoublyLinkedList;
