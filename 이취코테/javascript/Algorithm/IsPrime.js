ad36a6388d6108cfa71ee98070d443f86228731a
wish1030 <wish@khu.ac.kr>
2022-03-16 09:57:02 +0900

    [소수의 판별]

---
+++ 이취코테/javascript/Algorithm/IsPrime.js
@@ -0,0 +1,12 @@
+//x가 소수인지 아닌지 판별
+export default function is_prime_number(x) {
+  // 2부터 x의 제곱근까지 모든 수를 확인하며
+  for (let i = 2; i < parseInt(Math.sqrt(x)) + 1; i++) {
+    //x가 해당 수로 나누어떨어진다면
+    if (x % i === 0) return false; //소수가 아님
+    return true; //소수임
+  }
+}
+
+console.log(is_prime_number(4));
+console.log(is_prime_number(7));
