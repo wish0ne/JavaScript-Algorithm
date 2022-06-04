de507c160a6844476ccb38499005ecdeb593ac08
wish0ne <wish@khu.ac.kr>
2022-06-04 19:33:32 +0900

    [프로그래머스] 피로도

---
+++ 프로그래머스/Level2/피로도.js
@@ -0,0 +1,109 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  //연결리스트의 맨 뒤에 추가
+  append(value) {
+    const node = new Node(value);
+    //list가 비었을때
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    }
+    //list에 node가 존재할때
+    else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+    this.size += 1;
+    return this;
+  }
+
+  //연결리스트의 맨 앞을 삭제
+  deleteHead() {
+    //연결리스트가 비었을때
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    const deletedHead = this.head;
+    //리스트에 노드가 하나밖에 없을때
+    if (!this.head.next) {
+      this.head = null;
+      this.tail = null;
+    }
+    //리스트에 노드가 여러개인 경우
+    else {
+      this.head = this.head.next;
+      this.head.prev = null;
+    }
+    this.size -= 1;
+    return deletedHead.value;
+  }
+
+  delete(node) {
+    if (node === this.head) {
+      if (node === this.tail) {
+        this.head = null;
+        this.tail = null;
+      } else {
+        this.head = node.next;
+        node.next.prev = null;
+      }
+    } else if (node === this.tail) {
+      this.tail = node.prev;
+      node.prev.next = null;
+    } else {
+      node.prev.next = node.next;
+      node.next.prev = node.prev;
+    }
+    this.size -= 1;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+}
+
+function solution(k, dungeons) {
+  let answer = 0;
+  const q = new DoublyLinkedList();
+  let visited = new Array(dungeons.length).fill(false);
+  q.append([visited, k, 0]);
+  while (!q.isEmpty()) {
+    let [visited, fatigue, count] = q.deleteHead();
+    answer = Math.max(count, answer);
+    for (let i = 0; i < dungeons.length; i++) {
+      if (!visited[i] && dungeons[i][0] <= fatigue) {
+        let temp = [...visited];
+        temp[i] = true;
+        let nFatigue = fatigue - dungeons[i][1];
+        q.append([temp, nFatigue, count + 1]);
+      }
+    }
+  }
+  return answer;
+}
+
+//solve
+//bfs로 풀었는데 dfs나 이중for문으로도 가능한 문제(던전이 최대 8개밖에 안됨)
+
+console.log(
+  solution(50, [
+    [50, 30],
+    [40, 10],
+    [30, 20],
+  ])
+);
