23224a5e3b3f62311a49bd4fa31ba401cbf66bd3
wish0ne <wish@khu.ac.kr>
2022-09-07 15:58:53 +0900

    [백준] 11651 좌표 정렬하기2 (실5)

---
+++ 백준/정렬/11651.js
@@ -0,0 +1,20 @@
+import fs from "fs";
+const readFile = "../test.txt";
+// const readFile = '/dev/stdin';
+let input = fs.readFileSync(readFile).toString().trim().split("\n");
+
+const n = parseInt(input[0]);
+const points = [];
+for (let i = 1; i <= n; i++) points.push(input[i].split(" ").map(Number));
+points.sort((a, b) => {
+  if (a[1] !== b[1]) return a[1] - b[1];
+  else return a[0] - b[0];
+});
+
+let answer = "";
+points.forEach((p) => {
+  answer += p[0] + " " + p[1] + "\n";
+});
+console.log(answer);
+
+//solve
