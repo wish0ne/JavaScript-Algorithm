764028c2b915c2510cd0cc431e890d6214af29b8
wish0ne <wish@khu.ac.kr>
2022-09-17 13:31:26 +0900

    [백준] 2109 순회강연 (골3) Heap을 이용한 그리디 문제

---
+++ 백준/그리디/2109.js
@@ -0,0 +1,269 @@
+class Heap {
+  constructor() {
+    this.heap = [];
+  }
+
+  getLength() {
+    return this.heap.length;
+  }
+
+  //왼쪽 자식 인덱스 = 부모인덱스 * 2 + 1
+  getLeftChildIndex(parentIndex) {
+    return 2 * parentIndex + 1;
+  }
+
+  //오른쪽 자식 인덱스 = 부모인덱스 * 2 + 2
+  getRightChildIndex(parentIndex) {
+    return 2 * parentIndex + 2;
+  }
+
+  //부모인덱스 = ((자식인덱스 - 1) / 2)의 내림
+  getParentIndex(childIndex) {
+    return Math.floor((childIndex - 1) / 2);
+  }
+
+  //부모 노드가 있는지
+  hasParent(childIndex) {
+    return this.getParentIndex(childIndex) >= 0;
+  }
+
+  //왼쪽 자식 노드가 있는지
+  hasLeftChild(parentIndex) {
+    return this.getLeftChildIndex(parentIndex) < this.heap.length;
+  }
+
+  //오른쪽 자식 노드가 있는지
+  hasRightChild(parentIndex) {
+    return this.getRightChildIndex(parentIndex) < this.heap.length;
+  }
+
+  //왼쪽 자식 노드의 값
+  leftChild(parentIndex) {
+    return this.heap[this.getLeftChildIndex(parentIndex)];
+  }
+
+  //오른쪽 자식 노드의 값
+  rightChild(parentIndex) {
+    return this.heap[this.getRightChildIndex(parentIndex)];
+  }
+
+  //부모 노드의 값
+  parent(childIndex) {
+    return this.heap[this.getParentIndex(childIndex)];
+  }
+
+  // 두 노드의 값을 교환
+  swap(indexOne, indexTwo) {
+    const temp = this.heap[indexTwo];
+    this.heap[indexTwo] = this.heap[indexOne];
+    this.heap[indexOne] = temp;
+  }
+
+  //최상위 노드 return (min Heap에서 최솟값을 반환, max Heap에서 최대값을 반환)
+  peek() {
+    if (this.heap.length === 0) return null;
+    return this.heap[0];
+  }
+
+  //최우선순위 원소 제거
+  poll() {
+    if (this.heap.length === 0) return null;
+    if (this.heap.length === 1) return this.heap.pop();
+
+    const item = this.heap[0];
+
+    // 가장 마지막 원소를 맨 위로 이동
+    this.heap[0] = this.heap.pop();
+    this.heapifyDown();
+
+    return item;
+  }
+
+  add(item) {
+    this.heap.push(item);
+    this.heapifyUp();
+    return this;
+  }
+
+  remove(item) {
+    const numberOfItemsToRemove = this.find(item).length;
+
+    for (let iteration = 0; iteration < numberOfItemsToRemove; iteration += 1) {
+      const indexToRemove = this.find(item).pop();
+      if (indexToRemove === this.heap.length - 1) {
+        this.heap.pop();
+      } else {
+        this.heap[indexToRemove] = this.heap.pop();
+        const parentItem = this.parent(indexToRemove);
+        if (
+          (this.hasLeftChild(indexToRemove) && !parentItem) ||
+          this.pairIsInCorrectOrder(this.parentItem, this.heap[indexToRemove])
+        ) {
+          this.heapifyDown(indexToRemove);
+        } else {
+          this.heapifyUp(indexToRemove);
+        }
+      }
+    }
+    return this;
+  }
+
+  find(item) {
+    //찾고자 하는 아이템의 인덱스들
+    const foundItemIndices = [];
+
+    for (let itemIndex = 0; itemIndex < this.heap.length; itemIndex += 1) {
+      if (item === this.heap[itemIndex]) {
+        foundItemIndices.push(itemIndex);
+      }
+    }
+
+    return foundItemIndices;
+  }
+
+  isEmpty() {
+    return !this.heap.length;
+  }
+
+  toString() {
+    return this.heap.toString();
+  }
+
+  heapifyUp(customStartIndex) {
+    //마지막 원소를 heap이 될때까지(올바른 순서가 될때까지) 올림
+    //add에서는 파라미터 없이(맨 마지막 요소를 올림), remove에서는 파라미터 넣어서(그 요소를 올림) heapifyUp을 호출
+    let currentIndex = customStartIndex || this.heap.length - 1;
+
+    //부모 요소가 존재하고(올릴 곳이 있고) 부모 요소의 값과 현재값을 비교해서 바꿔야하면 swap
+    //swap했을 경우 현재 인덱스를 부모 인덱스로 업데이트
+    while (
+      this.hasParent(currentIndex) &&
+      !this.pairIsInCorrectOrder(
+        this.parent(currentIndex),
+        this.heap[currentIndex]
+      )
+    ) {
+      this.swap(currentIndex, this.getParentIndex(currentIndex));
+      currentIndex = this.getParentIndex(currentIndex);
+    }
+  }
+
+  heapifyDown(customStartIndex = 0) {
+    //디폴트 파라미터
+    //root부터 시작해서 부모 요소와 자식 요소를 비교해서 ( minheap에서는 더 작은 자식, maxheap에서는 더 큰 자식과) swap.
+    // 다음 자식에 대해서도 반복
+    let currentIndex = customStartIndex;
+    let nextIndex = null;
+
+    //자식이 존재할경우 반복
+    while (this.hasLeftChild(currentIndex)) {
+      //왼쪽 자식과 오른쪽 자식 둘다 존재하고, 오른쪽 자식의 순위가 더 높은 경우 (minheap인 경우 오른쪽이 더 작고 maxheap인 경우 오른쪽이 더 큰 경우) 오른쪽 자식을 다음 인덱스로 설정.
+      if (
+        this.hasRightChild(currentIndex) &&
+        this.pairIsInCorrectOrder(
+          this.rightChild(currentIndex),
+          this.leftChild(currentIndex)
+        )
+      ) {
+        //다음 인덱스는 오른쪽 자식
+        nextIndex = this.getRightChildIndex(currentIndex);
+      } else {
+        //왼쪽 자식이 있고 오른쪽 자식이 없거나, 오른쪽 자식이 있어도 왼쪽의 우선순위가 더 높을 경우 왼쪽 자식을 다음 인덱스로 설정.
+        nextIndex = this.getLeftChildIndex(currentIndex);
+      }
+
+      //현재 인덱스의 값이 다음 인덱스의 값과 비교했을때 올바른 순서인 경우 break
+      //max heap인 경우 현재 인덱스의 값이 다음 인덱스의 값보다 클 경우
+      //min heap인 경우 현재 인덱스의 값이 다음 인덱스의 값보다 작은 경우
+      if (
+        this.pairIsInCorrectOrder(this.heap[currentIndex], this.heap[nextIndex])
+      ) {
+        break;
+      }
+
+      //현재 인덱스의 값과 다음 인덱스의 값을 비교했을때 올바르지 않은 순서인 경우 swap
+      this.swap(currentIndex, nextIndex);
+      currentIndex = nextIndex; //현재 인덱스 업데이트
+    }
+  }
+
+  pairIsInCorrectOrder(firstElement, secondElement) {
+    // Max Heap일 경우
+    // 첫번째 인자가 두번째 인자보다 크거나 같아야함.
+    return firstElement >= secondElement;
+  }
+}
+
+import fs from "fs";
+const readFile = "../test.txt";
+// const readFile = '/dev/stdin';
+let input = fs.readFileSync(readFile).toString().split("\n");
+
+const n = parseInt(input[0]);
+if (n === 0) {
+  console.log(0);
+  process.exit();
+}
+const colleges = [];
+for (let i = 1; i <= n; i++) {
+  colleges.push(input[i].split(" ").map(Number));
+}
+
+colleges.sort((a, b) => b[1] - a[1]); //d가 큰 순으로 정렬
+
+let maxHeap = new Heap();
+
+let answer = 0;
+let idx = 0;
+//day
+for (let i = colleges[0][1]; i > 0; i--) {
+  while (idx < n && colleges[idx][1] >= i) {
+    maxHeap.add(colleges[idx][0]);
+    idx += 1;
+  }
+  answer += maxHeap.poll();
+}
+
+console.log(answer);
+
+//보석도둑과 매우 유사한 문제
+//처음에는 문제를 잘 못 이해해서 d일 이내에만 방문하면 되는건데 d일째에 방문해야한다고 잘 못 짰음..
+//그 다음에는 d를 큰순으로 정렬해서, 맨 마지막 날짜부터 1일까지 하나씩 확인하면서
+//해당 날짜에 가능한 학교들(heap에 저장)중 p가 최대인 것 더하는 알고리즘 생각
+//이 알고리즘을 잘 떠올렸으나 이를 한번에 깔끔하게 짜지 못하고 놓치는게 많았음...
+//머릿속으로 정리를 마친후에 코드로 작성하자!!
+
+//해설
+class Lecture {
+  constructor(p, d) {
+    this.p = p;
+    this.d = d;
+  }
+}
+
+const a = new Array(n);
+for (let i = 1; i <= n; i++) {
+  const [p, d] = input[i].split(" ").map(Number);
+  a[i - 1] = new Lecture(p, d);
+}
+
+a.sort((a, b) => b.d - a.d); //d가 큰 순으로 정렬
+
+let k = 0; //강연 index
+const q = new Heap();
+let ans = 0;
+for (let i = 10000; i >= 1; i--) {
+  //현재 날짜에서 갈 수 있는 강연 후보들 저장
+  //i일까지 할 수 있는 강연 저장
+  while (k < n && a[k].d === i) {
+    q.add(a[k].p);
+    k += 1;
+  }
+  //후보들 중 p가 최대인 강연 선택
+  //i일에 어떤 강연 할지 선택
+  if (q.getLength() !== 0) {
+    ans += q.poll();
+  }
+}
+
+console.log(ans);
