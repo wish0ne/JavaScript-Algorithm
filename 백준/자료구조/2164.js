30936fbcbf02765e91e1e3fdc1a8105b6c21ba2b
wish0ne <wish@khu.ac.kr>
2022-12-14 21:46:59 +0900

    [백준] 2164 카드2 (실4)

---
+++ 백준/자료구조/2164.js
@@ -0,0 +1,109 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  //연결리스트의 맨 뒤에 추가
+  append(value) {
+    const node = new Node(value);
+    //list가 비었을때
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    }
+    //list에 node가 존재할때
+    else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+    this.size += 1;
+    return this;
+  }
+
+  //연결리스트의 맨 앞을 삭제
+  deleteHead() {
+    //연결리스트가 비었을때
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    const deletedHead = this.head;
+    //리스트에 노드가 하나밖에 없을때
+    if (!this.head.next) {
+      this.head = null;
+      this.tail = null;
+    }
+    //리스트에 노드가 여러개인 경우
+    else {
+      this.head = this.head.next;
+      this.head.prev = null;
+    }
+    this.size -= 1;
+    return deletedHead.value;
+  }
+
+  delete(node) {
+    if (node === this.head) {
+      if (node === this.tail) {
+        this.head = null;
+        this.tail = null;
+      } else {
+        this.head = node.next;
+        node.next.prev = null;
+      }
+    } else if (node === this.tail) {
+      this.tail = node.prev;
+      node.prev.next = null;
+    } else {
+      node.prev.next = node.next;
+      node.next.prev = node.prev;
+    }
+    this.size -= 1;
+  }
+
+  print() {
+    let iter = this.head;
+    let print = "";
+    while (iter !== null) {
+      print += `${iter.value} `;
+      iter = iter.next;
+    }
+    console.log(print);
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+}
+
+import fs from "fs";
+const readFile = "./input.txt";
+// const readFile = '/dev/stdin';
+let input = fs.readFileSync(readFile).toString().split("\n");
+
+const n = parseInt(input[0]);
+
+const queue = new DoublyLinkedList();
+for (let i = 1; i <= n; i++) {
+  queue.append(i);
+}
+while (queue.size > 1) {
+  queue.deleteHead();
+  const v = queue.deleteHead();
+  queue.append(v);
+}
+console.log(queue.deleteHead());
+
+//solve
+//큐 자료구조 이용
